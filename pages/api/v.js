7ad36df0886fe8cf32a729f36827310323ffe969
pkellner <peter@peterkellner.net>
2024-08-23 10:56:52 -0700

    gox

---
+++ pages/api/v.js
@@ -0,0 +1,13 @@
+import Redis from 'ioredis'
+
+
+//const str = process.env.REDIS_URL;
+const str = 'redis://172.29.0.2:6379'
+//const str = 'redis://redisdata:6379'
+console.log("/pages/api/incr.js: str", str);
+
+export default async (req, res) => {
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  await sleep(1000)
+  res.status(200).json({ str })
+}
