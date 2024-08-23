9e0509f066a7ad8549677d626fff7d39b2dd8bc9
pkellner <peter@peterkellner.net>
2024-08-23 10:58:01 -0700

    x

--- pages/api/v.js
+++ pages/api/v.js
@@ -1,10 +1,10 @@
 import Redis from 'ioredis'
 
 
-//const str = process.env.REDIS_URL;
-const str = 'redis://172.29.0.2:6379'
+const str = process.env.REDIS_URL;
+//const str = 'redis://172.29.0.2:6379'
 //const str = 'redis://redisdata:6379'
-console.log("/pages/api/incr.js: str", str);
+//console.log("/pages/api/incr.js: str", str);
 
 export default async (req, res) => {
   const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
