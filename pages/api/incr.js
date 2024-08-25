d702735ee7da99f5e23ebbdf2a8b3597b4fa45c6
pkellner <peter@peterkellner.net>
2024-08-25 08:51:15 -0700

    xxxx

--- pages/api/incr.js
+++ pages/api/incr.js
@@ -2,8 +2,8 @@ import Redis from 'ioredis'
 
 console.log("/pages/api/incr.js: process.env.REDIS_URL", process.env?.REDIS_URL ?? 'no redis url');
 
-//const str = process.env.REDIS_URL;
-const str = 'redis://172.29.0.2:6379'
+const str = process.env.REDIS_URL;
+//const str = 'redis://172.29.0.2:6379'
 //const str = 'redis://redisdata:6379'
 console.log("/pages/api/incr.js: str", str);
 
