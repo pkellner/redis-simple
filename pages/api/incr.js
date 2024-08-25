ba7052a0f4379a930c1a176b22c9b429ae24d2a1
pkellner <peter@peterkellner.net>
2024-08-25 08:46:04 -0700

    x

--- pages/api/incr.js
+++ pages/api/incr.js
@@ -2,19 +2,21 @@ import Redis from 'ioredis'
 
 console.log("/pages/api/incr.js: process.env.REDIS_URL", process.env?.REDIS_URL ?? 'no redis url');
 
-const str = process.env.REDIS_URL;
-//const str = 'redis://172.29.0.2:6379'
+//const str = process.env.REDIS_URL;
+const str = 'redis://172.29.0.2:6379'
 //const str = 'redis://redisdata:6379'
 console.log("/pages/api/incr.js: str", str);
 
+let redis;
 try {
-  let redis = new Redis(str)
+  redis = new Redis(str)
 } catch (e) {
   console.log("/pages/api/incr.js: e", e);
 }
 
 
 export default async (req, res) => {
+  console.log("/pages/api/incr.js: about to incr");
   const count = await redis.incr('counter')
   res.status(200).json({ count })
 }
