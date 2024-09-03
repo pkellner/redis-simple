cffc2f85c3b9eb6b37efc7a7ccd45f15f1bfc0bd
pkellner <peter@peterkellner.net>
2024-09-03 06:23:17 -0700

    zzz

--- pages/api/v.js
+++ pages/api/v.js
@@ -6,8 +6,11 @@ const str = process.env.REDIS_URL;
 //const str = 'redis://redisdata:6379'
 //console.log("/pages/api/incr.js: str", str);
 
+const releaseDate = process.env?.RELEASEDATE ?? 'no release date';
+
 export default async (req, res) => {
   const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
   await sleep(1000)
-  res.status(200).json({ data: str ?? "NO_REDIS_URL", time: new Date().toISOString() })
+  const z = { RELEASEDATE: releaseDate ?? "NO RELEASEDATE", data: str ?? "NO_REDIS_URL", time: new Date().toISOString(), releaseDate: releaseData }
+  res.status(200).json(z);
 }
