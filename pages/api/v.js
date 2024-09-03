43f8286c9b648f00c6414d31603926f6d58a4123
pkellner <peter@peterkellner.net>
2024-09-03 06:27:25 -0700

    Z

--- pages/api/v.js
+++ pages/api/v.js
@@ -1,16 +1,22 @@
-import Redis from 'ioredis'
-
+import Redis from "ioredis";
 
 const str = process.env.REDIS_URL;
 //const str = 'redis://172.29.0.2:6379'
 //const str = 'redis://redisdata:6379'
 //console.log("/pages/api/incr.js: str", str);
 
-const releaseDate = process.env?.RELEASEDATE ?? 'no release date';
+const releaseDate = process.env?.RELEASEDATE ?? "no release date";
 
 export default async (req, res) => {
-  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-  await sleep(1000)
-  const z = { RELEASEDATE: releaseDate ?? "NO RELEASEDATE", data: str ?? "NO_REDIS_URL", time: new Date().toISOString(), releaseDate: releaseData }
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  await sleep(1000);
+  const z = {
+    RELEASEDATE: releaseDate ?? "NO RELEASEDATE",
+    RELEASEDATEISO: new Date(releaseDate)?.toISOString(),
+    RELEASEVERSION: process.env?.RELEASEVERSION ?? "NO RELEASEVERSION",
+    data: str ?? "NO_REDIS_URL",
+    time: new Date().toISOString(),
+    releaseDate: releaseData,
+  };
   res.status(200).json(z);
-}
+};
