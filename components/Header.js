82c788474ac02874baf84bf558b5382fd779d5c1
pkellner <peter@peterkellner.net>
2025-07-31 15:17:06 -0700

    probb

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>HEADER</h1>
+      <h1 className={styles.title}>HEADER  1b</h1>
     </header>
   )
 }
